be1d0a2b35fe8da457471f72ff75d05373178cca
Arinabut <arinabutorina958@gmail.com>
2024-05-20 14:16:24 +0400

    Add files via upload

---
+++ shar/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace PulsarApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 200;
+            animation.To = 100;
+            animation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            circle.BeginAnimation(Ellipse.WidthProperty, animation);
+
+            DoubleAnimation animation2 = new DoubleAnimation();
+            animation2.From = 200;
+            animation2.To = 100;
+            animation2.Duration = new Duration(TimeSpan.FromSeconds(1));
+            circle.BeginAnimation(Ellipse.HeightProperty, animation2);
+        }
+    }
+}
